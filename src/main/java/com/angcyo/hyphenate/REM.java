c9389bdf0d6c9ccbc35b2aada449788fdd7650c8
angcyo <angcyo@126.com>
2018-03-05 00:04:57 +0800

    fix base init

--- src/main/java/com/angcyo/hyphenate/REM.java
+++ src/main/java/com/angcyo/hyphenate/REM.java
@@ -2,13 +2,24 @@ package com.angcyo.hyphenate;
 
 import android.app.Application;
 
+import com.angcyo.library.utils.L;
+import com.angcyo.uiview.net.Func;
+import com.angcyo.uiview.net.RException;
+import com.angcyo.uiview.net.RSubscriber;
+import com.angcyo.uiview.net.Rx;
+import com.angcyo.uiview.utils.ThreadExecutor;
+import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMOptions;
+import com.hyphenate.exceptions.HyphenateException;
+
+import rx.Observable;
+import rx.Observer;
 
 /**
  * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
  * 项目名称：
- * 类的描述：
+ * 类的描述：环信SDK封装
  * 创建人员：Robi
  * 创建时间：2018/02/22 15:51
  * 修改人员：Robi
@@ -31,4 +42,59 @@ public class REM {
         //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
         EMClient.getInstance().setDebugMode(debug);
     }
+
+    public static Observable register(final String username, final String pwd) {
+        return Rx.create(new Func<String>() {
+            @Override
+            public String call(Observer observer) {
+                try {
+                    EMClient.getInstance().createAccount(username, pwd);
+                } catch (HyphenateException e) {
+                    observer.onError(e);
+                }
+                return username;
+            }
+        });
+    }
+
+    public static void login(final String username, final String pwd, final RSubscriber<String> subscriber) {
+        subscriber.onStart();
+        EMClient.getInstance().login(username, pwd, new EMCallBack() {//回调
+            @Override
+            public void onSuccess() {
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+
+                ThreadExecutor.instance().onMain(new Runnable() {
+                    @Override
+                    public void run() {
+                        subscriber.onCompleted();
+                        subscriber.onNext("");
+                    }
+                });
+            }
+
+            @Override
+            public void onProgress(int progress, String status) {
+                L.e("onProgress() -> " + progress + " " + status);
+                ThreadExecutor.instance().onMain(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onError(final int code, final String message) {
+                ThreadExecutor.instance().onMain(new Runnable() {
+                    @Override
+                    public void run() {
+                        subscriber.onError(new RException(code, message));
+                    }
+                });
+            }
+        });
+    }
+
 }

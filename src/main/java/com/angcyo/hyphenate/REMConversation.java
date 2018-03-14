dfffba33a273b5f6647df9b0628a0dcd382faf08
angcyo <angcyo@126.com>
2018-03-14 18:18:16 +0800

    fix base

--- src/main/java/com/angcyo/hyphenate/REMConversation.java
+++ src/main/java/com/angcyo/hyphenate/REMConversation.java
@@ -1,5 +1,10 @@
 package com.angcyo.hyphenate;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Pair;
 
 import com.hyphenate.chat.EMClient;
@@ -23,6 +28,14 @@ import java.util.Map;
  * Version: 1.0.0
  */
 public class REMConversation {
+
+    public static final String ACTION_GROUP_CHANAGED = "action_group_changed";
+    public static final String ACTION_CONTACT_CHANAGED = "action_contact_changed";
+    private LocalBroadcastManager broadcastManager;
+    private BroadcastReceiver broadcastReceiver;
+
+    private REMConversation() {
+    }
 
     /*根据消息的时间, 降序排序*/
     private static void sortConversationByLastChatTime(List<Pair<Long, EMConversation>> conversationList) {
@@ -88,5 +101,52 @@ public class REMConversation {
         //删除当前会话的某条聊天记录
         EMConversation conversation = EMClient.getInstance().chatManager().getConversation(username);
         conversation.removeMessage(msgId);
+    }
+
+    public static REMConversation instance() {
+        return Holder.instance;
+    }
+
+    /**
+     * 注册广播监听, 当会话改变
+     */
+    public void registerBroadcastReceiver(Context context) {
+        broadcastManager = LocalBroadcastManager.getInstance(context.getApplicationContext());
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_CONTACT_CHANAGED);
+        intentFilter.addAction(ACTION_GROUP_CHANAGED);
+        broadcastReceiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+//                updateUnreadLabel();
+//                updateUnreadAddressLable();
+//                if (currentTabIndex == 0) {
+//                    // refresh conversation list
+//                    if (conversationListFragment != null) {
+//                        conversationListFragment.refresh();
+//                    }
+//                } else if (currentTabIndex == 1) {
+//                    if (contactListFragment != null) {
+//                        contactListFragment.refresh();
+//                    }
+//                }
+//                String action = intent.getAction();
+//                if (action.equals(Constant.ACTION_GROUP_CHANAGED)) {
+//                    if (EaseCommonUtils.getTopActivity(MainActivity.this).equals(GroupsActivity.class.getName())) {
+//                        GroupsActivity.instance.onResume();
+//                    }
+//                }
+            }
+        };
+        broadcastManager.registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    public void unregisterBroadcastReceiver() {
+        broadcastManager.unregisterReceiver(broadcastReceiver);
+    }
+
+    private static class Holder {
+        static REMConversation instance = new REMConversation();
     }
 }

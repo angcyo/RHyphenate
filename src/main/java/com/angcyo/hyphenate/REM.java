4ad9b5dbb91360fa8f8725e1cbe407e370f84a83
angcyo <angcyo@126.com>
2018-03-21 15:43:12 +0800

    fix base

--- src/main/java/com/angcyo/hyphenate/REM.java
+++ src/main/java/com/angcyo/hyphenate/REM.java
@@ -6,6 +6,8 @@ import com.angcyo.uiview.net.Func;
 import com.angcyo.uiview.net.RException;
 import com.angcyo.uiview.net.RSubscriber;
 import com.angcyo.uiview.net.Rx;
+import com.hyphenate.EMConnectionListener;
+import com.hyphenate.EMError;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMOptions;
 import com.hyphenate.exceptions.HyphenateException;
@@ -40,6 +42,33 @@ public class REM {
         EMClient.getInstance().init(application, options);
         //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
         EMClient.getInstance().setDebugMode(debug);
+
+        registerConnectionListener();
+    }
+
+    private static void registerConnectionListener() {
+        //注册一个监听连接状态的listener
+        EMClient.getInstance().addConnectionListener(new EMConnectionListener() {
+            @Override
+            public void onConnected() {
+
+            }
+
+            @Override
+            public void onDisconnected(int error) {
+                if (error == EMError.USER_REMOVED) {
+                    // 显示帐号已经被移除
+                } else if (error == EMError.USER_LOGIN_ANOTHER_DEVICE) {
+                    // 显示帐号在其他设备登录
+                } else {
+//                    if () {
+//                        //连接不到聊天服务器
+//                    } else {
+//                        //当前网络不可用，请检查网络设置
+//                    }
+                }
+            }
+        });
     }
 
     /**
